ed520b255a9cb13701345ba73b34f682652a5eb7
java.praveenk <java.praveenk@gmail.com>
2016-01-28 22:49:12 +0530

    Multiple calls refactor.

--- NodeJsClient/clientservice0.js
+++ NodeJsClient/clientservice0.js
@@ -27,33 +27,24 @@ exports.batchHandler2 = function(req, res) {
     if(err) { console.log(err); res.send(500,"Server Error"); return; }
   var temp = results[0];
   var arr=[];
+  var getAll = '[';
     for(var i=0; i<temp.length; i++){
-      console.log('********');
-      console.log(temp[i].productId);
       var tempUrl = 'http://localhost:3001/getById?productId=';
       var url = tempUrl+temp[i].productId;
-      console.log(url);
-      arr.push("'"+url+"'");
+      arr.push(url);
     }
-  //  console.log('new arr'+arr);
 
-var tArr = '['+arr[0]+','+arr[1]+','+arr[2]+','+arr[3]+','+arr[4]+','+arr[5]+','+arr[6]+','+arr[7]+','+arr[8]+']';
-console.log('tArr : '+tArr);
-
-var abc = ['http://localhost:3001/getById?productId=P11111',
-     'http://localhost:3001/getById?productId=P11112',
-     'http://localhost:3001/getById?productId=P11113'] ;
-
-    async.map(tArr, function(item, callback){
+    async.map(arr, function(item, callback){
       request(item, function (error, response, body) {
         callback(error, body);
       });
     }, function(err, results){
-      console.log("merge:"+results);
+      console.log('********    merge      ***************************************');
+      console.log(results);
+      console.log('**************************');
+
       res.send(results);
     });
-
-
   }
   );
 };
